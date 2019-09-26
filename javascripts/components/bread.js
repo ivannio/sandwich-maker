d392ba01562701f69b7fe3b97464f5fab277aefd
Ivan Phelps <ivanniothemannio@gmail.com>
2019-09-25 20:37:44 -0500

    selected ingredients functions written

--- javascripts/components/bread.js
+++ javascripts/components/bread.js
@@ -6,3 +6,29 @@ const breads = [
     {id:"bread3", name: "Sourdough", price: 60}
 ];
 
+const getSelectedBreads = () => {
+    let selectedBreads = [];
+    const breadCheckboxes = document.getElementsByClassName("bread");
+    for (let i = 0; i < breadCheckboxes.length; i++) {
+        for (let j = 0; j < breads.length; j++) {
+            if (breadCheckboxes[i].checked && breadCheckboxes[i].id === breads[j].id) {
+                selectedBreads.push(breads[j]);
+            }
+          }
+        }
+    return selectedBreads;
+};
+
+const breadPrinter = () => {
+    let stringToPrint = '';
+    for (let i = 0; i < breads.length; i++) {
+        stringToPrint += `<div class="form-group form-check">
+        <input type="checkbox" class="form-check-input bread" id="${breads[i].id}">
+        <label class="form-check-label" for="exampleCheck1">${breads[i].name}</label>
+      </div>`;
+    }    
+    utilities.printToDom(stringToPrint, "bread-counter");
+};
+
+
+export default { breadPrinter, getSelectedBreads };
