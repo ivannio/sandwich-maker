17c19e4b3ba3085944a2a6e15e8999b4b1346dd2
Ivan Phelps <ivanniothemannio@gmail.com>
2019-09-25 20:46:54 -0500

    WIP

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
+import cheese from "./components/cheese.js";
+// import order from "./components/order.js";
+
 console.log("main.js is linked");
+
+const init = () => {
+    cheese.cheesePrinter();
+    order.printOrderButton();
+}
+
+init();
