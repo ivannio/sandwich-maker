d2822ba5365cca1b6ca524e880d5ce265a70273f
Ivan Phelps <ivanniothemannio@gmail.com>
2019-09-25 20:31:05 -0500

    ingredient arrays made

---
+++ javascripts/components/condiments.js
@@ -0,0 +1,10 @@
+import utilities from "../helpers/utilities.js";
+
+const condiments = [
+    {id:"condiment1", name: "Mayo", price: 30},
+    {id:"condiment2", name: "Mustard", price: 30},
+    {id:"condiment3", name: "Ranch", price: 30},
+    {id:"condiment4", name: "Honey Mustard", price: 30},
+    {id:"condiment5", name: "Chipotle Ranch", price: 30}
+];
+
